c7a94cd9f611588293c0a02fcac858a51fe929ef
Chase.Gaddis <cgaddis36@gmail.com>
2020-11-05 12:00:06 -0600

    reconfig routes to house navbar redirects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
+
+  get '/zip', to: 'sessions#update'
+  get '/forecast', to: 'forecast#show'
+  get '/fly', to: 'fly#index'
+  get '/bait', to: 'bait#index'
+  get '/tournament', to: 'tournament#index'
+  get '/tips', to: 'tips#index'
 end
