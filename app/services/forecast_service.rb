ae12d09e18946f3ef09540199d3ee9cb0ad76383
Chase.Gaddis <cgaddis36@gmail.com>
2020-11-16 16:50:06 -0600

    comment out tides connection

--- app/services/forecast_service.rb
+++ app/services/forecast_service.rb
@@ -3,10 +3,17 @@ class ForecastService
     response = conn.get("/forecasts/#{zip}", {filter: '1hr', client_id: ENV["AERIS_ACCESS_ID"], client_secret: ENV["AERIS_SECRET_KEY"]})
 
     parsed = JSON.parse(response.body)
+
     hourly_forecasts = parsed["response"][0]["periods"].map {|hourly| Forecast.new(hourly)}
 
     JSON.parse(hourly_forecasts.to_json)
   end
+
+  # def get_tides(zip)
+  #   response = conn.get("/tides/#{zip}", {client_id: ENV["AERIS_ACCESS_ID"], client_secret: ENV["AERIS_SECRET_KEY"]})
+  #   parsed = JSON.parse(response.body)
+  #
+  # end
 
   private
   def conn
