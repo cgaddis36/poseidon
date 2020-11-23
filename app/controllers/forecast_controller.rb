4e75805c361ac43262d49a76f5caa60cbffc2289
Chase.Gaddis <cgaddis36@gmail.com>
2020-11-23 10:39:22 -0600

    take next 48 hrs forecast

--- app/controllers/forecast_controller.rb
+++ app/controllers/forecast_controller.rb
@@ -1,5 +1,5 @@
 class ForecastController < ApplicationController
   def show
-    @forecast = ForecastService.new.get_forecast(session[:zip])[0..10]
+    @forecast = ForecastService.new.get_forecast(session[:zip])[0..47]
   end
 end
