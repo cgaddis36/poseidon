2b7d9816d0623711ee616643fa02cfb01a07fd80
Chase.Gaddis <cgaddis36@gmail.com>
2020-11-23 10:26:17 -0600

    add in avg_feels_like attribute to forecasts

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -5,6 +5,7 @@ class Forecast
               :humidity,
               :pop,
               :precip,
+              :avg_feels_like,
               :pressure,
               :wind_direction,
               :wind_speed,
@@ -18,12 +19,13 @@ class Forecast
     @temp = forecast["tempF"]
     @dewpoint = forecast["dewPointF"]
     @humidity = forecast["humidity"]
+    @avg_feels_like = forecast["avgFeelsLikeF"]
     @pop = forecast["pop"]
     @precip = forecast["precipIN"]
-    @pressure = forecast["pressureIN"]
+    @pressure = forecast["pressureMB"]
     @wind_direction = forecast["windDir"]
-    @wind_speed = forecast["windSpeedMPH"]
-    @wind_gusts = forecast["windGustMPH"]
+    @wind_speed = forecast["windSpeedKTS"]
+    @wind_gusts = forecast["windGustKTS"]
     @weather = forecast["weather"]
     @visibility = forecast["visibilityMI"]
     @icon = icon_finder(forecast["weather"])
