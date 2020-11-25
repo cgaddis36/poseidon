b2f49361750f3c848dee9faa7ee309aa26b05950
Chase.Gaddis <cgaddis36@gmail.com>
2020-11-25 10:43:38 -0600

    reconfig forecast PORO for scalability purposes

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -35,6 +35,7 @@ class Forecast
 
   def icon_finder(weather)
     possible_weather = ['clear',
+                        'sunny',
                         'mostly clear',
                         'raining',
                         'mostly sunny',
@@ -47,11 +48,29 @@ class Forecast
                         'partly cloudy with isolated showers',
                         'mostly cloudy with isolated showers',
                         'partly cloudy with isolated storms',
-                        'mostly cloudy with scattered showers']
+                        'mostly cloudy with scattered showers',
+                        'partly cloudy with patchy fog',
+                        'mostly cloudy with chance of light snow showers']
     image = []
     possible_weather.each do |forecast|
       if weather.downcase == forecast
         image = forecast.gsub(' ', '_') + ".jpg"
+      elsif weather.downcase.include?('snow')
+        image = 'general_snow.jpg'
+      elsif weather.downcase.include?('rain')
+        image = 'general_rain.jpg'
+      elsif weather.downcase.include?('showers')
+        image = 'general_rain.jpg'
+      elsif weather.downcase.include?('fog')
+        image = 'general_fog.jpg'
+      elsif weather.downcase.include?('thunder')
+        image = 'general_thunder.jpg'
+      elsif weather.downcase.include?('sun')
+        image = 'general_sun.jpg'
+      elsif weather.downcase.include?('cloud')
+        image = 'general_cloud.jpg'
+      elsif weather.downcase.include?('clear')
+        image = 'general_clear.jpg'
       end
     end
     image
