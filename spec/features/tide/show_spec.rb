7cdad7e50c6761daef2dec1ed5f07b9af998e70c
Chase.Gaddis <cgaddis36@gmail.com>
2020-11-25 12:23:11 -0600

    new spec file for getting tides data

---
+++ spec/features/tide/show_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.describe 'As a visitor when I enter a correct ZipCode in, and click Get Tides' do
+  it 'I am taken to the tides#show page where I can see all of my forecasted tides data' do
+    visit '/'
+
+
+  end
+end
