f15611a0f29e9103c2a777795ca507c91ec08b4b
Chase.Gaddis <cgaddis36@gmail.com>
2020-11-29 10:05:35 -0600

    tides service

--- app/services/tides_service.rb
+++ app/services/tides_service.rb
@@ -1,11 +1,11 @@
 class TidesService
   def get_tides(zip)
+    f = File.read('app/assets/stations/florida.txt')
 
+
+    require "pry"; binding.pry
   end
 
   private
-  def conn
-    Faraday.new(url: )
-  end
 
 end
