baf5905de4e605178cfa5ee9b0744d0e410e7fea
Chase.Gaddis <cgaddis36@gmail.com>
2020-11-29 10:05:40 -0600

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_28_173736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "stations", force: :cascade do |t|
+    t.string "location"
+    t.string "lat"
+    t.string "lon"
+    t.string "station_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
