ab4192355f51f9c02a93bafb7b9d90507d3b331b
Chase.Gaddis <cgaddis36@gmail.com>
2020-11-30 08:04:01 -0600

    remove unnecessary comments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 files = ['app/assets/stations/florida.txt', 'app/assets/stations/alabama.txt',
          'app/assets/stations/georgia.txt', 'app/assets/stations/alaska.txt',
          'app/assets/stations/california.txt', 'app/assets/stations/connecticut.txt',
