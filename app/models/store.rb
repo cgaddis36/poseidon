d466b85b5ace59bcc7c3e1cde1ecd0b9b0008d39
Chase.Gaddis <cgaddis36@gmail.com>
2020-12-15 11:23:07 -0600

    remove pry

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,7 +2,6 @@ class Store < ApplicationRecord
   validates :name, :state, :city, :zip, :address, :phone_number, presence: true
 
   def self.nearby_bait(state, zip)
-    require "pry"; binding.pry
     where(zip: zip)
   end
 end
