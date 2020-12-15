403284f0f5d0d548eb6a43533b3a616eabf4da54
Chase.Gaddis <cgaddis36@gmail.com>
2020-12-15 11:21:42 -0600

    remove tag validation

--- spec/models/store_spec.rb
+++ spec/models/store_spec.rb
@@ -8,6 +8,5 @@ RSpec.describe Store, type: :model do
     it { should validate_presence_of :state }
     it { should validate_presence_of :zip }
     it { should validate_presence_of :phone_number }
-    it { should validate_presence_of :tags }
   end
 end
