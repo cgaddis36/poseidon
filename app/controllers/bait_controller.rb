97a90016a4d2e27b68d0901d02220c7432b88949
Chase.Gaddis <cgaddis36@gmail.com>
2020-12-15 13:29:04 -0600

    remove unnecessary state argument in nearby_bait method

--- app/controllers/bait_controller.rb
+++ app/controllers/bait_controller.rb
@@ -1,6 +1,6 @@
 class BaitController < ApplicationController
   def index
-    @baits = Store.nearby_bait(session[:state], session[:zip])
+    @baits = Store.nearby_bait(session[:zip])
     if session[:zip].nil?
       flash[:error] = 'Please Enter a Correct Zipcode to continue'
       redirect_to '/'
