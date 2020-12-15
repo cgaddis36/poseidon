2643a6c833894573b5a5b31e212d92438ec575ad
Chase.Gaddis <cgaddis36@gmail.com>
2020-12-15 10:27:05 -0600

    create nearby_bait sotre method to find nearby stores based on zipcode

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,3 +1,7 @@
 class Store < ApplicationRecord
   validates :name, :state, :city, :zip, :address, :tags, :phone_number, presence: true
+
+  def self.nearby_bait(state, zip)
+    where(zip: zip)
+  end
 end
