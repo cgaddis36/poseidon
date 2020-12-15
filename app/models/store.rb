48fa8b7700ae4793c577628270051887a67750d1
Chase.Gaddis <cgaddis36@gmail.com>
2020-12-15 11:21:11 -0600

    remove fly and bait validations

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,7 +1,8 @@
 class Store < ApplicationRecord
-  validates :name, :state, :city, :zip, :address, :tags, :phone_number, presence: true
+  validates :name, :state, :city, :zip, :address, :phone_number, presence: true
 
   def self.nearby_bait(state, zip)
+    require "pry"; binding.pry
     where(zip: zip)
   end
 end
