8621982d376f2c50283e7c5134bb78243c8642c5
Chase.Gaddis <cgaddis36@gmail.com>
2020-12-17 09:21:00 -0600

    add activerecord call to only show public shops that have been reviewed

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,10 +2,10 @@ class Store < ApplicationRecord
   validates :name, :state, :city, :zip, :address, :phone_number, presence: true
 
   def self.nearby_bait(zip)
-    where(zip: zip).where(bait: true)
+    where(zip: zip).where(bait: true).where(public: true)
   end
 
   def self.nearby_fly(zip)
-    where(zip: zip).where(fly: true)
+    where(zip: zip).where(fly: true).where(public: true)
   end
 end
