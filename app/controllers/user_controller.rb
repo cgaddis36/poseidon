3d0ab39dc8290fa24aa211dcdb3eb49854010b7c
Chase.Gaddis <cgaddis36@gmail.com>
2020-12-17 13:47:18 -0600

    UserController addtional methods

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,24 @@
 class UserController < ApplicationController
   def new
   end
+
+  def show
+  end
+
+  def update
+    user = current_user.update(user_params)
+    if user
+      flash[:notice] = "#{current_user.user_name} successfully updated."
+      redirect_to '/user'
+    else
+      flash[:notice] = user.errors.full_messages.to_sentence
+      redirect_to request.referrer
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :zip, :email)
+  end
 end
