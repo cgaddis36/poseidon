440531375885752920a5efda1eba9364cbbf7b32
Chase.Gaddis <cgaddis36@gmail.com>
2020-12-17 11:59:03 -0600

    user schema migration updates for uniqueness on user_name attribute

--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_12_17_165055) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
