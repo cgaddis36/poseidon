caeaa70bbb0ef1f09df86f666428f3829db04456
Chase.Gaddis <cgaddis36@gmail.com>
2020-12-21 11:07:28 -0600

    route mvoement for legibility

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'welcome#index'
+  get '/about', to: 'about#index'
 
-  post 'store/:store_id/review', to: 'review#create'
-  get '/store/:store_id/review/new', to: 'review#new'
-  get '/store/:store_id', to: 'store#show'
   post '/store', to: 'store#create'
+  get '/store/:store_id', to: 'store#show'
+  get '/store/:store_id/review/new', to: 'review#new'
+  post 'store/:store_id/review', to: 'review#create'
 
+  get '/user', to: 'user#show'
   patch '/user', to: 'user#update'
   get '/user/edit', to: 'user#edit'
-  get '/user', to: 'user#show'
   post '/user', to: 'sessions#creation'
   get '/logout', to: 'sessions#destroy'
   get '/register', to: 'user#new'
@@ -21,13 +22,9 @@ Rails.application.routes.draw do
   get '/tides', to: 'tides#show'
   get '/forecast', to: 'forecast#show'
   get '/fly', to: 'fly#index'
-  get '/bait', to: 'bait#index'
-  get '/guide', to: 'guide#index'
-  get '/tournament', to: 'tournament#index'
-  get '/tips', to: 'tips#index'
-  get '/maps', to: 'maps#show'
-  get '/bait_search', to: 'bait#search'
   get '/fly_search', to: 'fly#search'
+  get '/bait', to: 'bait#index'
+  get '/bait_search', to: 'bait#search'
+  get '/guide', to: 'guide#index'
   get '/guide_search', to: 'guide#search'
-  get '/about', to: 'about#index'
 end
