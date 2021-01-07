e2c92521ae380650240297c3c875a5bcf4f4a5d2
Chase.Gaddis <cgaddis36@gmail.com>
2021-01-07 11:10:32 -0600

    add query for finding the closest station

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,7 +1,6 @@
 module Types
   class QueryType < Types::BaseObject
     field :all_stations, resolver: Queries::AllStations
-
-
+    field :closest_station, resolver: Queries::ClosestStation
   end
 end
