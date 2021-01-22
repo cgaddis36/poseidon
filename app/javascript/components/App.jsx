0154f0cba81e044a0ca9b0f8380fab7143f63f02
Chase.Gaddis <cgaddis36@gmail.com>
2021-01-22 17:37:44 -0600

    Tides components initial creation

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Welcome from './Welcome/Welcome'
 import Forecast from './Forecast/Forecast'
+import Tides from './Tides/Tides'
 
 const App = () => {
   return<Switch>
     <Route exact path="/" component={Welcome}/>
     <Route exact path="/forecast/:zipcode" component={Forecast}/>
+    <Route exact path="/tides/:zipcode" component={Tides}/>
 
   </Switch>
 }
---
+++ app/javascript/components/Tides/Tides.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tides = () => {
+
+  return<h2>ShoreCasts Tides Show Page!</h2>
+
+}
+
+export default Tides
