399b2ffd961ce0afd14bfc108d9d8c50338df33d
Chase.Gaddis <cgaddis36@gmail.com>
2021-04-18 11:47:40 -0500

    remove logic from application controller to tesrt csrf

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
   protect_from_forgery with: :null_session
-  skip_before_action :verify_authenticity_token
-
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
