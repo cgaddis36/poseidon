f5f7fe49da92c0930e9b8da802d3e215a89ef3e4
Chase.Gaddis <cgaddis36@gmail.com>
2021-04-18 01:10:50 -0500

    * origin

--- config/application.rb
+++ config/application.rb
@@ -33,7 +33,7 @@ module Poseidon
 
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins '*', 'http://localhost:3000'
+        origins '*'
         resource '*', headers: :any, methods: [:get, :post]
       end
     end
