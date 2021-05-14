ab3011a2e71c2e24d12ff68ea8260357e7257c1a
Chase.Gaddis <cgaddis36@gmail.com>
2021-05-14 13:31:04 -0500

    update station model for new api config

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,3 +1,3 @@
 class Station < ApplicationRecord
-  validates :url, :state, :station_id, :lat, :lon, :location, :pred_type, presence: true
+  validates :state, :station_id, :lat, :lon, :name, presence: true
 end
--- db/migrate/20201128173736_create_stations.rb
+++ db/migrate/20201128173736_create_stations.rb
@@ -1,12 +1,10 @@
 class CreateStations < ActiveRecord::Migration[5.2]
   def change
     create_table :stations do |t|
-      t.string :location
+      t.string :name
       t.string :lat
       t.string :lon
       t.string :station_id
-      t.string :url
-      t.string :pred_type
       t.string :state
 
       t.timestamps
