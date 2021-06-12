bd23c9bce74e4cbb5fa454b5cd89d458a648ac71
Chase.Gaddis <cgaddis36@gmail.com>
2021-06-12 12:10:23 -0500

    modify review model for new backend schema

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,8 @@
 class Review < ApplicationRecord
+  validates :title, presence: true
   validates :comment, presence: true
   validates :rating, presence: true
 
   belongs_to :user
-  belongs_to :store
+  belongs_to :business_service
 end
