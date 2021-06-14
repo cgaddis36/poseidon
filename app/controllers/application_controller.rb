109a562634d84774b090623a47f6da62df4cdfb5
Chase.Gaddis <cgaddis36@gmail.com>
2021-06-14 10:59:22 -0500

    remove null_session to prevent the authenticity token from being nulled away

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
-  protect_from_forgery with: :null_session
+  # protect_from_forgery with: :null_session
   helper_method :current_user, :state_converter
 
   def current_user
